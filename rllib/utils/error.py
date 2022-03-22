c6fcdda28189fb529a01215316bce61f753ab109
decypherdatalabs <46941460+decypherdatalabs@users.noreply.github.com>
2022-03-22 17:19:52 -0500

    Update error.py

--- rllib/utils/error.py
+++ rllib/utils/error.py
@@ -1,11 +1,11 @@
 from ray.rllib.utils.annotations import PublicAPI
 
 
-#@PublicAPI
-#class UnsupportedSpaceException(Exception):
-#    """Error for an unsupported action or observation space."""
-#
-#    pass
+@PublicAPI
+class UnsupportedSpaceException(Exception):
+    """Error for an unsupported action or observation space."""
+
+    pass
 
 
 @PublicAPI
